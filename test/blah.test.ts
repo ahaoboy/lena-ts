151ad6607daadc8fc7a8e047c318900ef3939a49
ahaoboy <504595380@qq.com>
2021-03-11 01:02:27 +0800

    add del test

--- test/blah.test.ts
+++ test/blah.test.ts
@@ -1,10 +1,3 @@
-import { sum } from '../src';
-
-describe('blah', () => {
-  it('works', () => {
-    expect(sum(1, 1)).toEqual(2);
-  });
-});
 import pipe from '../src/utils/pipe';
 import red from '../src/filters/red';
 pipe(document.createElement('canvas'), [[red, 0], red]);
