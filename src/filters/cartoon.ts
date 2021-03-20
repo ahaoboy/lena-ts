15dd070196edee51cfb9f890516f3848d6bcbc24
ahaoboy <504595380@qq.com>
2021-03-20 17:46:01 +0800

    fix: cartoon add amout

--- src/filters/cartoon.ts
+++ src/filters/cartoon.ts
@@ -1,7 +1,7 @@
 import pipe from '../utils/pipe';
 import { Filter } from '../type';
 import truncate from '../utils/truncate';
-const cartoon: Filter = (pixels) => {
+const cartoon: Filter = (pixels, amount = 125) => {
   const { width, height } = pixels;
   const imageEdge = pipe(pixels, [
     'grayscale',
@@ -14,12 +14,14 @@ const cartoon: Filter = (pixels) => {
   const rawBuffer = pixels.data;
   const edgeBuffer = imageEdge.data;
   for (let i = 0; i < width * height * 4; i += 4) {
-    outBuffer[i] = truncate(2 * (edgeBuffer[i] / 3 + rawBuffer[i] - 100) + 16);
+    outBuffer[i] = truncate(
+      2 * (edgeBuffer[i] / 3 + rawBuffer[i] - amount) + amount
+    );
     outBuffer[i + 1] = truncate(
-      2 * (edgeBuffer[i + 1] / 3 + rawBuffer[i + 1] - 100) + 16
+      2 * (edgeBuffer[i + 1] / 3 + rawBuffer[i + 1] - amount) + amount
     );
     outBuffer[i + 2] = truncate(
-      2 * (edgeBuffer[i + 2] / 3 + rawBuffer[i + 2] - 100) + 16
+      2 * (edgeBuffer[i + 2] / 3 + rawBuffer[i + 2] - amount) + amount
     );
     outBuffer[i + 3] = rawBuffer[i + 3];
   }
