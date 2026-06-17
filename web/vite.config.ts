c528ae06315c7a61d88f1342e289298f457b5967
ahaoboy <504595380@qq.com>
2026-06-17 11:37:37 +0800

    ci

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,26 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite"
+import react from "@vitejs/plugin-react"
+import { visualizer } from "rollup-plugin-visualizer"
 
+const isDev = process.env["NODE_ENV"] === "development"
+
+// https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-});
+  base: "/image-stitch/",
+  plugins: [
+    react(),
+    ...(process.env["ANALYZE"] ? [visualizer({ open: true, gzipSize: true })] : []),
+  ],
+  resolve: {
+    alias: isDev
+      ? {}
+      : {
+        react: "https://esm.sh/react@19",
+        "react-dom": "https://esm.sh/react-dom@19",
+        "@mui/material": "https://esm.sh/@mui/material@9?standalone",
+        "@mui/icons-material": "https://esm.sh/@mui/icons-material@9",
+        "@emotion/react": "https://esm.sh/@emotion/react@11",
+        "@emotion/styled": "https://esm.sh/@emotion/styled@11",
+      },
+  },
+})
